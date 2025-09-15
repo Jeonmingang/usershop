d3c2736c1f0ccfbf0b9b248f9189f6d92a4bef40
SAMKOREA <123339750+Jeonmingang@users.noreply.github.com>
2025-09-16 08:14:30 +0900

    Delete src/main directory

--- src/main/java/com/minkang/ultimate/usershop/util/Msg.java
+++
@@ -1,8 +0,0 @@
-package com.minkang.ultimate.usershop.util;
-
-import org.bukkit.ChatColor;
-public final class Msg {
-    public static String color(String s) {
-        return ChatColor.translateAlternateColorCodes('&', s == null ? "" : s);
-    }
-}
