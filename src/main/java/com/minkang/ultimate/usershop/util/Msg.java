d3f8edfdeb2ada2baee668dc580666d4510141ac
SAMKOREA <123339750+Jeonmingang@users.noreply.github.com>
2025-09-16 06:39:45 +0900

    Add files via upload

---
+++ src/main/java/com/minkang/ultimate/usershop/util/Msg.java
@@ -0,0 +1,8 @@
+package com.minkang.ultimate.usershop.util;
+
+import org.bukkit.ChatColor;
+public final class Msg {
+    public static String color(String s) {
+        return ChatColor.translateAlternateColorCodes('&', s == null ? "" : s);
+    }
+}
